1e91b278f40f85bcdbdbbc93d1e2e828f6ae2510
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-24 22:30:32 -0400

    can get parameter from

--- hello.py
+++ hello.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route("/bye")
+def bye():
+    return "Bye Bye"
+
+@app.route("/greet/<name>")
+def greeting(name):
+    return f"Hello {name}"
+
 if __name__ == '__main__':
     app.run()
