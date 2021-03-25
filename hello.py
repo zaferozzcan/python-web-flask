16844081e3a6af0e74d0461768e70f5871bcd200
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-24 22:55:58 -0400

    add more route

--- hello.py
+++ hello.py
@@ -13,7 +13,7 @@ def bye():
 
 @app.route("/greet/<name>")
 def greeting(name):
-    return f"Hello {name}"
+    return f"<h1>Hello {name}</h1>"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
